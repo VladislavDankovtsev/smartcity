b75d135b297d522db7796a3b1bcd2f17e53bce90
Vldaislav_19 <vladislav.dankovtsev@yandex.ru>
2020-04-03 16:47:07 +0300

    обновлнние сущности производство и транспорт, sql script

--- src/main/java/ru/dankovtsev/smartcity/model/Production.java
+++ src/main/java/ru/dankovtsev/smartcity/model/Production.java
@@ -10,12 +10,16 @@ import java.util.UUID;
 
 @Entity
 @Table(name="production")
-public class Production  {
+public class Production {
     @Id
     @Column(name = "id")
     private UUID id;
     @Column(name="created_timestamp", insertable = true, updatable = false)
     private LocalDateTime time;
+    @Column(name="manipulator1")
+    private Integer manipulator1;
+    @Column(name="manipulator2")
+    private Integer manipulator2;
     @Column(name="status")
     private String status;
 
@@ -44,5 +48,32 @@ public class Production  {
 
     public void setStatus(String status) {
         this.status = status;
+    }
+
+    public Integer getManipulator1() {
+        return manipulator1;
+    }
+
+    public void setManipulator1(Integer manipulator1) {
+        this.manipulator1 = manipulator1;
+    }
+
+    public Integer getManipulator2() {
+        return manipulator2;
+    }
+
+    public void setManipulator2(Integer manipulator2) {
+        this.manipulator2 = manipulator2;
+    }
+
+    @Override
+    public String toString() {
+        return "Production{" +
+                "id=" + id +
+                ", time=" + time +
+                ", manipulator1=" + manipulator1 +
+                ", manipulator2=" + manipulator2 +
+                ", status='" + status + '\'' +
+                '}';
     }
 }
